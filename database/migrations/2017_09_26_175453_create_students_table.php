6031d6793442367db4adf702866aae4ecfa06c52
Ali Gandomi <gandomi313@gmail.com>
2017-09-26 21:50:42 +0330

    Change priority columns to one column

--- database/migrations/2017_09_26_175453_create_students_table.php
+++ database/migrations/2017_09_26_175453_create_students_table.php
@@ -20,22 +20,8 @@ class CreateStudentsTable extends Migration
             $table->string('stu_num');
             $table->string('field');
             $table->text('memo')->nullable();
-            $table->integer('priority_1')->unsigned()->nullable();
-            $table->integer('priority_2')->unsigned()->nullable();
-            $table->integer('priority_3')->unsigned()->nullable();
-            $table->integer('priority_4')->unsigned()->nullable();
-            $table->integer('priority_5')->unsigned()->nullable();
-            $table->integer('priority_6')->unsigned()->nullable();
-            $table->integer('priority_7')->unsigned()->nullable();
+            $table->json('priorities');
             $table->timestamps();
-
-            $table->foreign('priority_1')->references('id')->on('teachers');
-            $table->foreign('priority_2')->references('id')->on('teachers');
-            $table->foreign('priority_3')->references('id')->on('teachers');
-            $table->foreign('priority_4')->references('id')->on('teachers');
-            $table->foreign('priority_5')->references('id')->on('teachers');
-            $table->foreign('priority_6')->references('id')->on('teachers');
-            $table->foreign('priority_7')->references('id')->on('teachers');
         });
     }
 
